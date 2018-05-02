fafe9cb86542c356be8cef8cf9f3ffa3c676d89d
Andreas Daskalopoulos <adaskalopoulos@velti.com>
2018-05-02 14:54:58 +0300

    Created user successfully

--- src/app/mock-data/mock-users.ts
+++ src/app/mock-data/mock-users.ts
@@ -2,8 +2,4 @@ import {User} from '../model/user';
 
 const avatar = 'https://www.shareicon.net/data/128x128/2016/08/05/807323_gaming_512x512.png';
 
-export const USERS: User[] = [{
-  _id: '1', name: 'Mr. Nice', avatar: avatar, birthday: '23', country: 'Greece', appsCount: 6, apps: [{
-    _id: 'xyz', name: 'Jega', avatar: avatar
-  }]
-}];
+export const USERS: User[] = [];
